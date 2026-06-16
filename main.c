926097988b2669c41672c8089a99b62ac26ed44c
David Canizares <scaniizares@gmail.com>
2026-06-15 20:14:19 -0500

    Commit inicial

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ =============== UNIVERSIDAD DE LAS AMÉRICAS ===============
+        FACULTAD DE INGENIERÍA Y CIENCIAS APLICADAS
+                INGENIERÍA DE SOFTWARE
+                    PROGRAMACIÓN I
+Autores:
+    -David Cañizares
+    -Sebastián Orbe
+    -Jean Pierre Rosas
+    -Derek Yépez
+
+=========== KANBAN PARA GESTIÓN DE TAREAS =============
+
+
+*/
